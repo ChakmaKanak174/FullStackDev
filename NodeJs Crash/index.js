fd95480a839c3484b592b3db209c9ab2a67107cf
ChakmaKanak174 <kanakchakma195@gmail.com>
2025-03-26 02:09:45 +0200

    ES module export import

--- NodeJs Crash/index.js
+++ NodeJs Crash/index.js
@@ -1,3 +1,12 @@
-const generateRandomNumber = require('./utils');
+import getPosts, { getPostLength } from "./postController.js";
 
-console.log(`Random Number: ${generateRandomNumber()}`);
+console.log(getPosts());
+console.log(`Post Length: ${getPostLength()}`);
+
+
+
+// const { generateRandomNumber, CelToFar } = require('./utils');
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+// console.log(`Celsius to Farenhite: ${CelToFar(0)}`);
+
--- NodeJs Crash/utils.js
+++ NodeJs Crash/utils.js
@@ -2,4 +2,7 @@ function generateRandomNumber() {
     return Math.floor(Math.random() * 100) + 1;
 }
 
-module.exports = generateRandomNumber;
+function CelToFar(celcius) {
+    return (celcius * 9) / 5 + 32;
+}
+module.exports = { generateRandomNumber, CelToFar };
