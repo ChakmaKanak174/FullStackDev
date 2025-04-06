9979862b286e6a5aeb92cfbf9881506b1860016f
ChakmaKanak174 <kanakchakma195@gmail.com>
2025-04-06 21:27:06 +0300

    Postman extension added, done simple routing used try catch

--- NodeJs Crash/server.js
+++ NodeJs Crash/server.js
@@ -24,15 +24,40 @@ const server = http.createServer((req, res) => {
     // res.setHeader('Content-Type', 'text/html');
     // res.statusCode = 404;
 
+    try {
+        //check if the request is a get request
+        if (req.method === 'GET') {
+            if (req.url === '/') {
+                res.writeHead(200, { 'content-type': 'text/html' });
+                // res.end(JSON.stringify({message : 'Server Error'})); //Does the same thing as res.write()
+                res.end('<h1>Homepage</h1>');
+                
+            } else if (req.url === '/about') {
+                res.writeHead(200, { 'content-type': 'text/html' });
+                res.end('<h1>About</h1>');
+            } else {
+                res.writeHead(404, { 'content-type': 'text/html' });
+                res.end('<h1>Not Found</h1>');
+            }
+        } else {
+            throw new Error('Method not allowed');
+        }
+    } catch (error) {
+        res.writeHead(500, { 'content-type': 'text/plain' });
+        res.end('Server Error');
+        
+    }
 
-    console.log(req.url);
-    console.log(req.method);
+
+
+    
+
+    // console.log(req.url);
+    // console.log(req.method);
     
     
 
-    res.writeHead(200, { 'content-type': 'text/html' });
-    // res.end(JSON.stringify({message : 'Server Error'})); //Does the same thing as res.write()
-    res.end('<h1>Hello World</h1>');
+    
 });
 
 server.listen(PORT, () => {
