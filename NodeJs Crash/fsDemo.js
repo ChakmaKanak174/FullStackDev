cc700fc296d31dc1fb2306585c3351deeca083bf
ChakmaKanak174 <kanakchakma195@gmail.com>
2025-04-08 21:12:43 +0300

    Filse system module ongoing, Readfile methods done

---
+++ NodeJs Crash/fsDemo.js
@@ -0,0 +1,37 @@
+// import fs from 'fs';
+
+import fs from 'fs/promises';
+
+// readFile() -- callback
+
+// fs.readFile('./test.txt', 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// })
+
+// Readfilesync() -- synchronous version
+
+// const data = fs.readFileSync('./test.txt', 'utf-8');
+// console.log(data);
+
+// readFile() -- Promise .then()
+
+// fs.readFile('./test.txt', 'utf-8')
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log(err));
+    
+
+// readFile() -- async/await
+
+const readFile = async () => {
+    try {
+        const data = await fs.readFile('./test.txt', 'utf-8')
+        console.log(data);
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+readFile();
--- NodeJs Crash/server2.js
+++ NodeJs Crash/server2.js
@@ -56,6 +56,26 @@ const notFoundHandler = (req, res) => {
 }
 
 
+// Route handler for POST /api/users
+
+const createUserHandler = (req, res) => {
+    let body = '';
+    // listen for data
+
+    req.on('data', (chunk) => {
+        body += chunk.toString()
+    });
+
+    req.on('end', () => {
+        const newUser = JSON.parse(body);
+        users.push(newUser);
+        res.statusCode = 201;
+        res.write(JSON.stringify(newUser));
+        res.end();
+    });
+}
+
+
 const server = createServer((req, res) => {
     logger(req, res, () => {
         jsonMiddleware(req, res, () => {
@@ -63,7 +83,10 @@ const server = createServer((req, res) => {
                 getUserHandler(req, res);
             } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
                 getUserById(req, res);
-            } else {
+            } else if (req.url === '/api/users' && req.method === 'POST') { 
+                createUserHandler(req, res);
+            }
+            else {
                 notFoundHandler(req, res);
             }
         })
