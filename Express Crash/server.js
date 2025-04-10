6c3ec5e78361e811c9437e21d8ed881aa39d3bdb
ChakmaKanak174 <kanakchakma195@gmail.com>
2025-04-10 06:45:17 +0300

    get single json value and all json value, limitting json value using query strings done

--- Express Crash/server.js
+++ Express Crash/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-
+const port = process.env.PORT || 8000;
 
 const app = express();
 
 // setup static folder in case of having lot of html files that need to be routed one by one
 
-app.use(express.static(path.join(__dirname, 'public'), {extensions: ['html']} ))
+// app.use(express.static(path.join(__dirname, 'public'), {extensions: ['html']} ))
 
 
 // no need to manually check the req.method like in the base node
@@ -22,5 +22,35 @@ app.use(express.static(path.join(__dirname, 'public'), {extensions: ['html']} ))
 //     res.send('About');
 // })
 
-app.listen(8000, () => console.log(`Server is running on port 8000`)
+let posts = [
+    { id: 1, title: 'Post one' },
+    { id: 2, title: 'Post two' },
+    {id: 3, title: 'Post three'}
+]
+
+
+// get all posts 
+app.get('/api/posts', (req, res) => {
+    
+    const limit = parseInt(req.query.limit);
+
+    if (!isNaN(limit) && limit > 0) {       // need to be very carefull in handling datatype because someone might want to inject SQL queries
+        res.json(posts.slice(0, limit));
+    }
+    else {
+        res.send(posts);
+    }
+    
+});
+
+
+// get single post
+app.get('/api/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id); // id string is pares into integer. req.params take the param from the url
+    res.json(posts.filter((post) => post.id === id)); // posts array is filtered and checked if the ID matches with the ID from URL
+})
+
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`)
 )
