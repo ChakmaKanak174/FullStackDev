ff67ffecf76aa6a167e40b6858e501f3f99b7fe0
ChakmaKanak174 <kanakchakma195@gmail.com>
2025-04-10 07:08:03 +0300

    status code, query string for limit, error handled, reduced code size

--- Express Crash/server.js
+++ Express Crash/server.js
@@ -35,11 +35,11 @@ app.get('/api/posts', (req, res) => {
     const limit = parseInt(req.query.limit);
 
     if (!isNaN(limit) && limit > 0) {       // need to be very carefull in handling datatype because someone might want to inject SQL queries
-        res.json(posts.slice(0, limit));
+        return res.status(200).json(posts.slice(0, limit));
     }
-    else {
-        res.send(posts);
-    }
+    
+    res.status(200).json(posts);
+
     
 });
 
@@ -47,7 +47,14 @@ app.get('/api/posts', (req, res) => {
 // get single post
 app.get('/api/posts/:id', (req, res) => {
     const id = parseInt(req.params.id); // id string is pares into integer. req.params take the param from the url
-    res.json(posts.filter((post) => post.id === id)); // posts array is filtered and checked if the ID matches with the ID from URL
+    
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+        return res.status(404).json({msg: `The post with id ${id} was not found`})
+    }
+
+    res.status(200).json(post);
 })
 
 
