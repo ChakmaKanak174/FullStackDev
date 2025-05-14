116dd7c7a9d98b712b3c20daf7877b9b3540db6f
ChakmaKanak174 <kanakchakma195@gmail.com>
2025-05-14 14:49:06 +0300

    addjob and delete job fuction complete, used react-Toast to show notification, it has some flaws, couldn;t fix it

--- react-crash2/src/App.jsx
+++ react-crash2/src/App.jsx
@@ -15,6 +15,7 @@ import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
 
 const App = () => {
+  // Add new job
   const addJob = async (newJob) => {
     const res = await fetch("/api/jobs", {
       method: "POST",
@@ -27,13 +28,25 @@ const App = () => {
     return;
   };
 
+  // Delete job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: "DELETE",
+    });
+    return;
+  };
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
-        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route
+          path="/jobs/:id"
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
---
+++ react-crash2/src/pages/EditJobPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditJobPage = () => {
+  return <div>edit</div>;
+};
+
+export default EditJobPage;
